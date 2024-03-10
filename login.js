8e72cc10c138b187153ccd4923eafcfc721cf3c7
Thameem-20 <ahmedthameem20@gmail.com>
2024-03-10 22:24:32 +0530

    completed

---
+++ login.js
@@ -0,0 +1,8 @@
+angular.module('myApp',[]);
+app.controller('LoginController', function($scope){
+    $scope.user = {username:",password:"};
+    $scope.isLoggedIn = false;
+    $scope.login = function(){
+        $scope.isLoggedIn = true; 
+};
+});
