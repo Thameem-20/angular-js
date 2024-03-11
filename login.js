7a12969d18bc8b26d586736c2363542586acf03d
Thameem-20 <ahmedthameem20@gmail.com>
2024-03-11 06:28:45 +0530

    error resolved

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
-angular.module('myApp',[]);
+var app = angular.module('myApp',[]);
 app.controller('LoginController', function($scope){
-    $scope.user = {username:",password:"};
+    $scope.user = {username:"username"};
     $scope.isLoggedIn = false;
     $scope.login = function(){
         $scope.isLoggedIn = true; 
